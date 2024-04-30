36044627451a1f2c0a69459f45e55e242d2d1631
unknown <Inholland@LT00-CI318239.intranet.inholland.nl>
2024-04-30 14:20:17 +0200

    first push

---
+++ chapeau1.4/ChapeauDAL/BaseDao.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ChapeauDAL
+{
+    public abstract class BaseDao
+    {
+        private SqlDataAdapter adapter;
+        protected SqlConnection conn;
+
+        public BaseDao()
+        {
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ChapeauDatabase"].ConnectionString);
+            adapter = new SqlDataAdapter();
+        }
+
+        protected SqlConnection OpenConnection()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed || conn.State == ConnectionState.Broken)
+                {
+                    conn.Open();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            return conn;
+        }
+
+        private void CloseConnection()
+        {
+            conn.Close();
+        }
+
+        /* For Insert/Update/Delete Queries */
+        protected void ExecuteEditQuery(string query, SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                adapter.InsertCommand = command;
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+        }
+
+        /* For Select Queries with sql parameters */ //this one returns based on an id or smth like that
+        protected DataTable ExecuteSelectQueryWithParameters(string query, params SqlParameter[] sqlParameters)
+        {
+            SqlCommand command = new SqlCommand();
+            DataTable dataTable;
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.Parameters.AddRange(sqlParameters);
+                command.ExecuteNonQuery();
+                adapter.SelectCommand = command;
+                adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return dataTable;
+        }
+
+        protected DataTable ExecuteSelectQuery(string query)      //this returns without the need of a parameter, e.g. table id
+        {
+            SqlCommand command = new SqlCommand();
+            DataTable dataTable;
+            DataSet dataSet = new DataSet();
+
+            try
+            {
+                command.Connection = OpenConnection();
+                command.CommandText = query;
+                command.ExecuteNonQuery();
+                adapter.SelectCommand = command;
+                adapter.Fill(dataSet);
+                dataTable = dataSet.Tables[0];
+            }
+            catch (SqlException e)
+            {
+                throw new Exception(e.Message);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return dataTable;
+        }
+    }
+}
